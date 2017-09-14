b982846060d0052d946685489ac8f8a5e2976b87
GabriRa <safrio2697@hotmail.com>
2017-09-14 16:04:19 +0200

    Comentado

--- ejer/express1.js
+++ ejer/express1.js
@@ -1,9 +1,9 @@
 var express = require("express");
 
-var app = express();
+var app = express(); //Crea  una app
 
-app.get("/home", (req, res) => {
-    res.end("Hello World!")
+app.get("/home", (req, res) => { //Espera una llamada del tipo GET en la ruta /home
+    res.end("Hello World!") //Envia una string al acabar la respuesta
 })
 
-app.listen(process.argv[2])
+app.listen(process.argv[2]) //Escucha un puerto dado por la linea de comandos
