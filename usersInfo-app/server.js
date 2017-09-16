277596e7f8bbdcbf238005927919b195e11ba69a
GabriRa <safrio2697@hotmail.com>
2017-09-16 20:12:08 +0200

    Server with the User's info app

---
+++ usersInfo-app/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) =>{
+  let resultado = {
+    ipAdress : req.headers["x-forwarded-for"].split(",")[0],
+    language : req.headers["accept-language"].split(",")[0],
+    software: req.headers["user-agent"]
+  };
+  res.end(JSON.stringify(resultado))
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
